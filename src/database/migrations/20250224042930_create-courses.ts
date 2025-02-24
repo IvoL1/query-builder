8198f748a7deb8b57453532672a3c37115867253
Ivo <ivoluis239@mail.com>
2025-02-24 01:49:40 -0300

    📦 feat: Create migration for the courses table

--- src/database/migrations/20250224042930_create-courses.ts
+++ src/database/migrations/20250224042930_create-courses.ts
@@ -1,10 +1,15 @@
-import type { Knex } from "knex";
-
+import { table } from 'console';
+import type { Knex } from 'knex';
+import { text } from 'stream/consumers';
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('courses', (table) => {
+    table.increments('id').primary(), // ID único
+      table.text('name').notNullable(), // Nome do curso
+      table.timestamp('created_at').defaultTo(knex.fn.now()); //Data de criação automática
+  });
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('courses');
 }
-
