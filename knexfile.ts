b3b355e9ca39a54525cce9dfad56b46f7198da5e
Ivo <ivoluis239@mail.com>
2025-02-24 22:46:25 -0300

    🗑️  feat: add HTTP method to delete course module

--- knexfile.ts
+++ knexfile.ts
@@ -1,18 +1,18 @@
 export default {
-  client: 'sqlite3',
-  connection: { filename: './src/database/database.db' }, // Define o arquivo do banco
+  client: "sqlite3",
+  connection: { filename: "./src/database/database.db" }, // Define o arquivo do banco
   pool: {
     afterCreate: (connection: any, done: any) => {
-      connection.run('PRAGMA  foreign_keys = ON', done);
+      connection.run("PRAGMA  foreign_keys = ON", done);
     },
   },
   useNullAsDefault: true, // Necessário para SQLite
   migrations: {
-    extension: 'ts',
-    directory: './src/database/migrations',
+    extension: "ts",
+    directory: "./src/database/migrations",
   },
   seeds: {
-    extension: 'ts',
-    directory: './src/database/seeds',
+    extension: "ts",
+    directory: "./src/database/seeds",
   },
 };
--- src/server.ts
+++ src/server.ts
@@ -1,47 +1,53 @@
-import express, { Request, Response } from 'express';
-import { knex } from './database/knex';
+import express, { Request, Response } from "express";
+import { knex } from "./database/knex";
 
 const app = express();
 const PORT = 3333;
 app.use(express.json());
 
-app.post('/courses', async (req: Request, res: Response) => {
+app.post("/courses", async (req: Request, res: Response) => {
   const { name } = req.body;
 
-  await knex('courses').insert({
+  await knex("courses").insert({
     name: name,
   });
   res.status(201).json({ name });
 });
 
-app.get('/courses', async (req: Request, res: Response) => {
+app.get("/courses", async (req: Request, res: Response) => {
   // const courses = await knex.raw('SELECT * FROM courses') usando comandos SQL
-  const courses = await knex('courses').select();
+  const courses = await knex("courses").select();
   res.json(courses);
 });
 
-app.put('/courses/:id', async (req: Request, res: Response) => {
+app.put("/courses/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
   const { name } = req.body;
-  await knex('courses').update({ name }).where({ id });
+  await knex("courses").update({ name }).where({ id });
   res.json();
 });
 
-app.delete('/courses/:id', async (req: Request, res: Response) => {
+app.delete("/courses/:id", async (req: Request, res: Response) => {
   const { id } = req.params;
-  await knex('courses').delete().where({ id });
+  await knex("courses").delete().where({ id });
   res.json();
 });
 
-app.post('/modules', async (req: Request, res: Response) => {
+app.post("/modules", async (req: Request, res: Response) => {
   const { course_id, name } = req.body;
-  await knex('course_modules').insert({ name, course_id });
+  await knex("course_modules").insert({ name, course_id });
   res.status(201).json();
 });
 
-app.get('/modules', async (req: Request, res: Response) => {
-  const courses = await knex('course_modules').select();
+app.get("/modules", async (req: Request, res: Response) => {
+  const courses = await knex("course_modules").select();
   res.json(courses);
+});
+
+app.delete("/modules/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await knex("course_modules").delete().where({ id });
+  res.json();
 });
 
 app.listen(PORT, () => {
